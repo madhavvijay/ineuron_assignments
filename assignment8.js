961630415cdaac27e319fe23e4fadd025904af8e
Madhav Vijayvargiya <81529634+madhavvijay@users.noreply.github.com>
2024-05-22 17:32:39 +0530

    Add files via upload

---
+++ assignment8.js
@@ -0,0 +1,23 @@
+//1. Can we put duplicate values in the set object ?
+//Ans: No
+
+//2. Write the syntax for
+// a) Creating new set object
+
+// const letters = new Set(["a", "b", "c"]);
+// console.log(letters);
+
+// b) Adding new element to set object
+// letters.add('d');
+// console.log(letters);
+
+// c) Deleting element from set object
+// letters.delete('a');
+// console.log(letters);
+// 3. Create a set object with four random numbers from 0 to 10. Check if this
+// newly created set object has 8 in it.Use set object methods.
+
+
+// const numbers = new Set([3, 6, 8, 5]);
+// let n1 = numbers.has('8');
+// console.log(n1);
